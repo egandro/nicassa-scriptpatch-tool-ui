fae4eea58909d932a945c65240c25e5f42c04aa6
Harald Fielker <harald.fielker@advancedqms.com>
2018-01-12 20:56:48 +0100

    added dummy tabs

--- src/app/views/dashboard/dashboard.module.ts
+++ src/app/views/dashboard/dashboard.module.ts
@@ -4,8 +4,15 @@ import { ChartsModule } from 'ng2-charts/ng2-charts';
 import { DashboardComponent } from './dashboard.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+
 @NgModule({
   imports: [
+    BsDropdownModule.forRoot(),
+    ModalModule.forRoot(),
+    TabsModule,
     DashboardRoutingModule,
     ChartsModule
   ],
--- webpack.config.js
+++ webpack.config.js
@@ -63,7 +63,9 @@ const isProd = (process.env.NODE_ENV === 'production');
 
 //add all external css to be added in our index.html--> like as if it's .angular-cli.json
 const styles = [
-  "./src/scss/style.scss"
+  "./src/scss/style.scss",
+  "./node_modules/font-awesome/css/font-awesome.css",
+  "./node_modules/simple-line-icons/css/simple-line-icons.css",
 ];
 
 //we add all our external scripts we want to load externally, like inserting in our index.html --> like as if it's .angular-cli.json
